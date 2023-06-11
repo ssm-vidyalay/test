0c23f1c2212ebf71118ae82266678a1c173376bb
Swapan <swapan.jain.cd.met20@itbhu.ac.in>
2023-06-11 22:09:10 +0530

    removed authentication on ba and ma

--- routes/course_MA_routes.js
+++ routes/course_MA_routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { getCourseMA, getCourseMAAll, addCourseMA, updateCourseMA, deleteCourseMA } = require("../controllers/course_MA_controllers");
 const router = express.Router();
-const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+// const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 
-router.route("/courses/ma/:id").get(isAuthenticatedUser, getCourseMA);
-router.route("/courses/ma/").get(isAuthenticatedUser, getCourseMAAll);
-router.route("/courses/ma/new").post(isAuthenticatedUser, authorizeRoles("admin"), addCourseMA);
-router.route("/courses/ma/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateCourseMA);
-router.route("/courses/ma/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteCourseMA);
+router.route("/courses/ma/:id").get(getCourseMA);
+router.route("/courses/ma/").get(getCourseMAAll);
+router.route("/courses/ma/new").post( addCourseMA);
+router.route("/courses/ma/:id").put( updateCourseMA);
+router.route("/courses/ma/:id").delete( deleteCourseMA);
 
 module.exports = router;
