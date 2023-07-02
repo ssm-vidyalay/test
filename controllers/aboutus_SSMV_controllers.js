77cdaa5e4be7efb969f23de3ffe0fa48459ef978
ssm-vidyalay <ssmvidyalay@gmail.com>
2023-07-02 12:55:01 +0530

    settingVariableNameOfcourswes

--- controllers/aboutus_SSMV_controllers.js
+++ controllers/aboutus_SSMV_controllers.js
@@ -2,65 +2,65 @@ const Aboutus = require("../models/aboutus_SSMV_models");
 
 // single getAboutusSSMV
 exports.getAboutusSSMV = async (req, res, next) =>{
-    const ssmv = await Aboutus.findById(req.params.id);
+    const data = await Aboutus.findById(req.params.id);
   res.status(200).json({
     success: true,
-    ssmv,
+    data,
   });
 }
 
 // All getAboutusSSMV
 exports.getAboutusSSMVAll = async (req, res, next) =>{
-    const ssmv = await Aboutus.find();
-  res.status(200).json({
+    const data = await Aboutus.find();
+    res.status(200).json({
     success: true,
-    ssmv,
+    data,
   });
 }
 
 // Add addAboutusSSMV 
 exports.addAboutusSSMV = async (req, res) =>{
-    const { description, images } = req.body;
+    const { description, imageurl } = req.body;
     const aboutus = {
         description: description,
         imageurl: imageurl,
     }
-    const ssmv = await Aboutus.create(aboutus);
+    const data = await Aboutus.create(aboutus);
 
     res.status(200).json({
         success: true,
-        ssmv,
+        data,
     });
 }
 
 // Upadate upadateAboutusSSMV
 exports.updateAboutusSSMV = async (req, res, next) =>{
-  let ssmv = await Aboutus.findById(req.params.id);
+  let data = await Aboutus.findById(req.params.id);
 
-  ssmv = await Aboutus.findByIdAndUpdate(req.params.id, req.body, {new:true,
+  data = await Aboutus.findByIdAndUpdate(req.params.id, req.body, {new:true,
     useFindAndModify:true,
     runValidators:true
   });
 
   res.status(200).json({
     success: true,
-    ssmv,
+    data,
 });
 }
 
 // delete deleteAboutusSSMV
 exports.deleteAboutusSSMV = async(req, res, next) =>{
 
-  let ssmv = await Aboutus.findById(req.params.id);
+  let data = await Aboutus.findById(req.params.id);
 
-  if(!ssmv){
+  if(!data){
     return res.status(500).json({
       success:false,
       message:"SSMV not found"
     })
   }
 
-  ssmv = await Aboutus.findByIdAndDelete(req.params.id, req.body, {new:true,
+  data = await Aboutus.findByIdAndDelete(req.params.id, req.body, {new:true,
     useFindAndModify:true,
     runValidators:true
   });
